136086c861c831fbc9a583472434b6ef4a6951ab
Arthur Santos <108244695+Its-JustMe@users.noreply.github.com>
2024-04-08 15:28:07 +0000

    Adicionando regras de validação

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -2,14 +2,33 @@ import { data } from '../helpers/data';
 import { formData } from '../helpers/interfaces';
 import { homeModel } from '../models/homeModels';
 import { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator'
+import { body, validationResult } from 'express-validator';
 
 export default {
+  /**
+   * Regras de validação de dados do usuário
+  */
+  validationRules: [
+    body(
+      'nome_cliente'
+    ).notEmpty().trim().isAlpha(
+      'pt-BR', 
+      { ignore: [' ', '-' ] }
+    ).withMessage('Insira um nome válido'),
+
+    body(
+      'email_cliente'
+    ).notEmpty().trim().isEmail().withMessage('Insira um endereço de email válido'),
+
+    body(
+      'assunto_cliente'
+    ).trim().escape().notEmpty().withMessage('Não pode enviar uma mensagem vazia')
+  ],
   /**
    * Função que renderiza no navegador a página home.
    * @param req Request
    * @param res Response
-   */
+  */
   renderHome: (req: Request, res: Response): void => {
     res.render(
       'pages/index', 
